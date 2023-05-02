292b2b1fff5f9e2d379066f79cdf71e8375d11be
domricciardiphoto <domricciardiphotro@gmail.com>
2023-05-02 16:43:55 -0400

    version4
      Please enter the commit message for your changes. Lines starting

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const path = require('path')
 const {autoUpdater} = require('electron-updater');
 const log = require('electron-log');
 log.transports.file.resolvePath = () => path.join('C:/Users/domin/Desktop/domcell', 'logs/main.log');
+log.log("Application version "+ app.getVersion())
 log.info("Hello, log");
 log.warn("Some problem appears");
 let win;
@@ -13,24 +14,39 @@ function createWindow() {
   win.loadFile(path.join(__dirname, 'index.html'))
 }
 
-app.on('ready' , () => {
-    createWindow()
-    autoUpdater.checkForUpdatesAndNotify()
 
+
+autoUpdater.on("update-available" , (info) => {
+  log.info("update available")
 })
 
-autoUpdater.on("update-available" , () => {
-  log.info("update-available")
+autoUpdater.on("update-not-available" , (info) => {
+  log.info("update not available")
+})
+
+autoUpdater.on('error' , (err) => {
+  log.info('Error in auto-updater. ' + err)
+})
+
+
+autoUpdater.on("download-progress" , (progressTrack) => {
+log.info("\n\ndownload-progress")
+log.info(progressTrack)
 })
 
 autoUpdater.on("checking-for-update" , () => {
-  log.info("checking-for-update")
+  log.info("checking for update")
 })
 
 autoUpdater.on("download-progress" , () => {
-  log.info("download-progress")
+  log.info("download progress")
 })
 
 autoUpdater.on("update-downloaded" , () => {
-  log.info("update-downloaded")
+  log.info("update downloaded")
+})
+
+app.on('ready' , () => {
+  createWindow()
+  autoUpdater.checkForUpdatesAndNotify()
 })
