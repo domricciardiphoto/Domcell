46778545c2cb9bc5d708be92e9f9e4f07725ba71
Dominick Ricciardi <advdom@Dominicks-Mac-Pro.local>
2023-08-03 12:04:54 -0400

    Mac build features and information

--- main.js
+++ main.js
@@ -354,12 +354,47 @@ app.on('ready', () => {
 
 
 
-
+const isMac = process.platform === 'darwin'
 
 
 
 const menu = [
+  ...(isMac ? [{
+    label: 'DOMCELL',
+    submenu: [{
+        role: 'services'
+      },
+      {
+        type: 'separator'
+      },
 
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+        {
+          type: 'separator'
+        },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  }] : []),
   {
     label: 'Home',
     click: gohome
