b655f518e0ca9d7273957ef86dd16c6cb1cb472b
domricciardiphoto <domricciardiphoto@gmail.com>
2024-01-23 08:28:41 -0500

    new builder

--- global.js
+++ global.js
@@ -3,7 +3,7 @@ var elements = document.getElementsByClassName('version');
 
 
 for (let element of elements) {
-  element.textContent = "Domcell 2023 Ver. 7.5.1 incl. badge CSS Ver. 12";
+  element.textContent = "Domcell 2023 Ver. 7.5.3 incl. badge CSS Ver. 12";
 }
 
 
